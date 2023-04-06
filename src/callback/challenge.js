f432f80f70082a6acc21bfe6e3d24c85628eb925
AndreaBluMun <lauralme96@gmail.com>
2023-04-06 14:40:46 -0500

    fetchdata

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -10,9 +10,16 @@ function fetchData(urlApi, callback) {
     xhttp.onreadystatechange = function (event){
     // Estado de la peticion 
       if  (xhttp.readyState === 4) {
+    // Retorna el estado de la peticion y valida si es 200
         if (xhttp.status === 200) {
-
+          callback(null, JSON.parce(xhttp.resposeText));
         }
+      } else {
+        const error = new Error('Error' + urlApi)
+        return callback(error, null);
       }
     }
+    xhttp.send();
 }
+
+fetchData(`${API}`)
